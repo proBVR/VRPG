c5abcbb7a872879148a426db062316356c7975b5
rei-akuto <akutoyuuki@gmail.com>
2018-11-02 21:50:03 +0900

    簡単なローカルストレージ作成

---
+++ ProjectResearchB/Assets/Scripts/localstrage/LocalStrage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+
+
+public class LocalStrage : MonoBehaviour {
+
+    //string myJson = "[{\"NUM\":\"1\",\"TEXT\":\"HELLO\"},{\"NUM\":\"2\",\"TEXT\":\"BONJOUR\"},]";
+    string dataName = "data.txt";
+
+    void Start()
+    {
+        LoadLocalStageData();
+    }
+
+    // ローカルストレージから読み込み
+    public void LoadLocalStageData()
+    {
+        string savePath = GetPath();
+
+        // ディレクトリが無い場合はディレクトリを作成して保存
+        if (!Directory.Exists(savePath))
+        {
+            // ディレクトリ作成
+            Directory.CreateDirectory(savePath);
+            // ローカルに保存
+            SaveToLocal(myJson);
+        }
+        else
+        {
+            // ローカルからデータを取得
+            JsonNode json = LoadFromLocal();
+            Debug.Log(json);
+        }
+    }
+
+    // 保存
+    void SaveToLocal(string json)
+    {
+        // jsonを保存
+        File.WriteAllText(GetPath() + dataName, json);
+    }
+
+    // 取得
+    JsonNode LoadFromLocal()
+    {
+        // jsonを読み込み
+        string json = File.ReadAllText(GetPath() + dataName);
+        JsonNode jnode = JsonNode.Parse(json);
+
+        return jnode;
+    }
+
+    // パス取得
+    string GetPath()
+    {
+        return Application.persistentDataPath + "/AppData/";
+    }
+}
