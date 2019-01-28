b5e59fae9d8252059b2908df7939fa7643ae6420
DESKTOP-MUA8S6O\rui <meranazunx@gmail.com>
2019-01-28 10:51:11 +0900

    準備画面の調整等

---
+++ ProjectResearchB/Assets/Scripts/LockCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mycamera;
+    private readonly Vector3 pos = new Vector3(0, 2.5f, 0);
+
+    private void Update()
+    {
+        transform.position += pos - mycamera.position;
+    }
+}
--- ProjectResearchB/Assets/Scripts/Others/SettingController.cs
+++ ProjectResearchB/Assets/Scripts/Others/SettingController.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Valve.VR;
 
-public class SettingController : MonoBehaviour {
+public class SettingController : MonoBehaviour
+{
     [SerializeField]
-    public Transform  leftHand, rightHand;
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Fire1"))
+    private Transform leftHand, rightHand;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Input._default.inActions.Teleport.GetStateDown(SteamVR_Input_Sources.RightHand))
         {
             var firstlength = Vector3.Distance(leftHand.position, rightHand.position);
             if (firstlength > 0)
             {
                 UserCamera.length = firstlength;
-               SceneManager.LoadScene("GameScene");
+                SceneManager.LoadScene("GameScene");
             }
 
             ////debug用
